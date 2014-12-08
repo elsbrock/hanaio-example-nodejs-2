861dee85ce5ddec13ed550e2cc5f4efdef0c79fb
root <root@025401c59b5d.(none)>
2014-12-08 12:48:54 +0000

    Bootstrap

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+var app = require('express')();
 
+app.get('/*', function (req, res) {
+    console.log('GET /' + req.params[0]);
+    
+    res.sendFile(req.params[0] ? req.params[0] : 'index.html', { root : './client'});
+});
+
+app.listen(8080);
